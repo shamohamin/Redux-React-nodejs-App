4f3ee4313544d6176bb96cace3ed62922ab6d646
Amin <mohamd@mohamds-MacBook-Pro.local>
2019-08-03 15:55:43 +0430

    version 0.2

--- src/Register.js
+++ src/Register.js
@@ -14,6 +14,7 @@ import {connect} from 'react-redux';
 import postRequest from './postRequest';
 import { bindActionCreators } from '../../../Library/Caches/typescript/3.5/node_modules/@types/react-redux/node_modules/redux';
 
+
 export class  Register extends React.Component {
 
     constructor(props) {
@@ -24,12 +25,24 @@ export class  Register extends React.Component {
             last_name : "" ,
             age : Number ,
             phone_number : Number ,
-            gender : ""
-        }
+            gender : "",
+            errors : {
+                first_name_err : "",
+                last_name_err : "",
+                age_err : "" ,
+                gender_err : "",
+                phone_number : ""
+            }
+        };
 
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this) ;
+        this.onChange = this.onChange.bind(this) ;
+        this.handelValidate = this.handelValidate.bind(this) ;
+        this.changeClassName = this.changeClassName.bind(this) ;
+
     }
+
+
 
 
     clear = () => {
@@ -42,53 +55,153 @@ export class  Register extends React.Component {
         ReactDOM.findDOMNode(age).value = "" ;
         var phone_number = document.getElementById('phone_number') ;
         ReactDOM.findDOMNode(phone_number).value = "" ;
+
     }
 
     onChange(event){
-        console.log(event.target.value);
-        this.setState({[event.target.name] : event.target.value});
+        
+        this.setState({[event.target.name] : event.target.value} , () => {
+            this.handelValidate();
+        }) ;
+
+    }
+    
+
+    handelValidate(){
+
+        let errors = {
+            first_name_err : "A" ,
+            last_name_err : "A" ,
+            age_err : "A" ,
+            gender_err : "A" ,
+            phone_number : "A"
+        } ;
+        let isValid = true ;
+        let first_name = this.state.first_name;
+        let pattern = /^[a-zA-Z]+$/;
+
+        
+        if(parseInt(first_name.length) === 0){
+            isValid = false ;
+            errors["first_name_err"] = "Cant Be Empty";
+        }
+
+        if(typeof errors["first_name_err"] !== "undefined"){
+            if(!pattern.test(first_name)){
+                isValid = false ;
+                errors["first_name_err"] = " It Must Be letters " ;
+                //console.log(errors["first_name_err"])
+            }
+        }
+
+        if(this.state.last_name.length === 0){
+            isValid = false ;
+            errors["last_name_err"] = "Cant Be Empty"
+        }
+        if(typeof errors["last_name_err"] !== "undefined"){
+            if(!this.state.last_name.match(/^[a-zA-Z]+$/)){
+                isValid = false ;
+                errors["last_name_err"] = "It must Be letters"
+            }
+        }
+
+        if(this.state.age.length === 0 ){
+            isValid = false ;
+            errors["age_err"] = "It Cant Be Empty"
+        }
+
+        if(typeof errors["age_err"] !== "undefined"){
+            if(!(/^[0-9]+$/).test(this.state.age)){
+                isValid = false ;
+                errors["age_err"] = " Must Be Number " ;
+            }
+        }
+
+        if(this.state.phone_number.length === 0){
+            isValid = false ;
+            errors["phone_number"] = "it Cant Be Empty";
+        }
+
+        if(typeof errors["phone_number"] !== 'undefined'){
+            if(!(/[0-9]+$/).test(this.state.phone_number)){
+                isValid = false ;
+                errors["phone_number"] = "Must Be Number" ;
+            }
+        }
+
+        this.setState({errors : errors },()=>{
+            console.log(this.state);
+        })
+        return isValid ;
+
     }
 
+    componentDidUpdate = () => {
+        //this.handelValidate();
+        console.log("updated")
+    }
 
     onSubmit = (event) => {
 
         event.preventDefault();
-        const data = {
-            first_name : this.state.first_name ,
-            last_name : this.state.last_name ,
-            age : this.state.age , 
-            gender : this.state.gender,
-            phone_number : this.state.phone_number
+
+        if(this.handelValidate()){
+            
+            const data = {
+                first_name : this.state.first_name ,
+                last_name : this.state.last_name ,
+                age : this.state.age , 
+                gender : this.state.gender,
+                phone_number : this.state.phone_number
+            }
+            this.props.postRequest(data);
+            this.clear();
+
+
+        }else{
+
+            alert("You Fucked Man ")
+            
         }
-        this.props.postRequest(data);
-        this.clear();
+
+    }
+
+    changeClassName(len){
+
+        if(len === 0){
+            return "form-control" ;
+        }else if(len > 1){
+            return "form-control is-invalid" ;
+        }else if(len === 1){
+            return "form-control is-valid" ; 
+        }
 
     }
 
 
     render(){
-        console.log(this.props);
         return (
             <div className="divBorder">
                 <form onSubmit={this.onSubmit} className="needs-validation" noValidate autoComplete="off">
-                    <div className="form-row form-control-large">
-                        <div className="row-md-4 mb-3">
+                    <div className="form-col form-control-large">
+                        <div className="row-md-4 mb-3 is-valid">
                             <label htmlFor="fist_name" className="col-form-label-lg">First_name</label>
                             <input 
                                 name = "first_name"
                                 type="text" 
-                                className="form-control" 
+                                className = {this.changeClassName(parseInt(this.state.errors.first_name_err.length))} 
                                 id="first_name"
                                 placeholder="first_name"
                                 onChange = {e => this.onChange(e)}
                                 required 
                             />
-                        <div className="valid-feedback">
+                            <hr/>
+                            <div className="valid-feedback">
                                 Looks Good !
-                        </div>
-                        <div className="invalid-feedback">
-                                Please Enter Your First_name !
-                        </div>
+                            </div>
+                            <div className="invalid-feedback" >
+                                {this.state.errors.first_name_err}
+                            </div>
                         </div>
                         <Spacer grow='0.08' />
                         <div className="row-md-5 mb-4">
@@ -96,18 +209,19 @@ export class  Register extends React.Component {
                             <input
                                 name = "last_name"
                                 type="text" 
-                                className="form-control"
+                                className={this.changeClassName(parseInt(this.state.errors.last_name_err.length))}
                                 id="last_name"
                                 placeholder="last_name"
                                 onChange = {e => this.onChange(e)}
                                 required
                                 />
+                                <hr/>
                             <div className="valid-feedback">
                                 Looks Good !
-                        </div>
-                        <div className="invalid-feedback">
-                                Please Enter Your Last_name !
-                        </div>
+                            </div>
+                            <div className="invalid-feedback">
+                                {this.state.errors.last_name_err}
+                            </div>
                         </div>
                         <Spacer grow='0.1' width='20px'/>
                         <div className="row-md-4 mb-3">
@@ -115,15 +229,19 @@ export class  Register extends React.Component {
                             <input 
                                 name="age"
                                 type="text"
-                                className="form-control"
+                                className={this.changeClassName(this.state.errors.age_err.length)}
                                 id="age"
                                 placeholder="Age"
                                 onChange = {e => this.onChange(e)}
                                 required
                                 />
-                            <div className="invalid-feedback">
+                            <div className="valid-feedback">
                                 Please Enter Your Age !
-                        </div>
+                            </div>
+                            <div className="invalid-feedback" >
+                                {this.state.errors.age_err}
+                            </div>
+                        <hr/>
                         <br/>
                         <FormLabel component="legend">Gender</FormLabel>
                                 <RadioGroup
@@ -141,18 +259,26 @@ export class  Register extends React.Component {
                                     label="(Disabled option)"
                                     />
                                 </RadioGroup>
+                                <hr/>
                             <div className="row-md-4 mb-3">
                                 <label htmlFor="phone_number" className="col-form-large-lg">Phone_number</label>
                                 <input
                                     name="phone_number" 
                                     type="text" 
-                                    className="form-control"
+                                    className={this.changeClassName(this.state.errors.phone_number.length)}
                                     placeholder="Phone_number"
                                     id="phone_number"
                                     onChange = {e => this.onChange(e)}
                                     required
                                     />
+                                <div className="valid-feedback">
+                                    Looks Good !
+                                </div>
+                                <div className="invalid-feedback">
+                                    {this.state.errors.phone_number}
+                                </div>
                             </div>
+                            <hr/>
                             <div className="btn-group">
                                 <input type="submit" className="btn btn-success" />
                                 <button className="btn btn-danger" onClick={this.clear}>Clear</button>           
@@ -160,7 +286,6 @@ export class  Register extends React.Component {
                         </div>
                     </div>
                 </form>
-
             </div>
         );
     }
